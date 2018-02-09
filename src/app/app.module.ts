706af4ebcf28053c4af98b80d896fd8e990d1c15
Yang Kim <yang.kim@jeunessehq.com>
2018-02-09 16:07:35 -0700

    Fixed the component error in app.module and added last component file

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,11 @@ import { HeaderComponent } from './header/header.component';
 
 import { AppComponent } from './app.component';
 import { RecipesComponent } from './recipes/recipes.component';
-import { RecipeListComponent } from './receipes/recipe-list/recipe-list.component';
 import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
 import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component';
+import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
 
 
 @NgModule({
@@ -17,10 +18,11 @@ import { ShoppingListComponent } from './shopping-list/shopping-list.component';
     AppComponent,
     HeaderComponent,
     RecipesComponent,
-    RecipeListComponent,
     RecipeDetailComponent,
     RecipeItemComponent,
-    ShoppingListComponent
+    RecipeListComponent,
+    ShoppingListComponent,
+    ShoppingEditComponent
   ],
   imports: [
     BrowserModule,
