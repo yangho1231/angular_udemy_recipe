9e96d4324674330842a8a961db7e5f4a22cc28d6
Yang Kim <yang.kim@jeunessehq.com>
2018-02-13 13:02:42 -0700

    Added sample item description to show up

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,7 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-
+  recipes: Recipe[] = [
+    new Recipe('A test Recipe', 'This is simplyh a test', 'https://images.media-allrecipes.com/userphotos/250x250/4567827.jpg')
+  ];
   constructor() { }
 
   ngOnInit() {
